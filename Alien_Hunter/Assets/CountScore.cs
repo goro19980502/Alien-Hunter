285575ae33e9cfb16a00597ecd727c61816a9d43
Shawn Hsieh <goro19980502@gmail.com>
2018-12-14 18:41:14 +0800

    first

---
+++ Alien_Hunter/Assets/CountScore.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountScore : MonoBehaviour {
+
+    int sum = 0;
+    GameObject[] eleobj;
+    void Update () {
+        if (eleobj == null)
+            eleobj = GameObject.FindGameObjectsWithTag("Monster");
+        
+	}
+    void OnCollisionEnter(Collision collision)
+    {
+        bool x = false;
+        for(int i = 0; i < eleobj.Length; i++)
+        {
+            if(collision.gameObject.name == eleobj[i].name)
+            {
+                eleobj[i].SetActive(false);
+                x = true;
+                break;
+            }
+        }
+    }
+    void scoreCount()
+    {
+        bool x = false;
+        if(eleobj.Length > 0)
+        {
+            for(int i = 0; i < eleobj.Length; i++)
+            {
+                if(eleobj[i].activeInHierarchy == true)
+                {
+                    x = true;
+                    break;
+                }
+            }
+        }
+        if(x == false)
+        {
+            Application.LoadLevel(1);
+        }
+    }
+}
---
+++ Alien_Hunter/Assets/timecount.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timecount : MonoBehaviour {
+
+    public float startTime;
+    public Text text;
+    string s;
+	void Update () {
+        showtime();
+	}
+    void showtime()
+    {
+        float nowTime = Time.time - startTime;
+        int timeInt = 180 - (int)nowTime;
+        Debug.Log("執行時間"+timeInt);
+        s = timeInt.ToString();
+        text.text = "Time： " + s;
+        if (timeInt == 0)
+            Application.LoadLevel(1);
+    }
+    //更新遊戲時間
+    void rest()
+    {
+        startTime = Time.time;
+    }
+    //時間暫停
+    public void OnEnableStop()
+    {
+        Time.timeScale = 0f;
+    }
+    //時間以正常速度運行
+    public void OnDisableStart()
+    {
+        Time.timeScale = 1f;
+    }
+}
